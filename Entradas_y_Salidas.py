9cc91bb7484fde273189d247739b78a0cba61931
Mateo Canzian <144487654+MateCanzian@users.noreply.github.com>
2024-08-22 23:29:35 -0300

    Add files via upload

---
+++ Entradas_y_Salidas.py
@@ -0,0 +1,8 @@
+numero_1 = float(input("Ingrese un valor\n"))  # ingresa un valor flotante para la variable a aquí
+numero_2 = float(input("Ingrese un segundo valor\n"))  # ingresa un valor flotante para la variable b aquí
+print("Suma:", numero_1 + numero_2)  # mostrar el resultado de la suma aquí
+print("Resta:", numero_1 - numero_2)  # mostrar el resultado de la resta aquí
+print("Multiplicación:", numero_1 * numero_2)  # mostrar el resultado de la multiplicación aquí
+print("División:", round(numero_1 / numero_2, 2))  # mostrar el resultado de la división aquí
+print("\n¡Este es mi segundo código ISI!")
+
